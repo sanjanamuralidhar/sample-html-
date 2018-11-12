12cd8cb291d0eb9ef121d5c9d2ef0e4099cc96f6
sanjanamuralidhar <44828329+sanjanamuralidhar@users.noreply.github.com>
2018-11-12 16:12:45 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,13 @@
+for a in range(0,4):
+  print(a)
+
+
+fruits=["grapes","orange","apple","lemon"]
+for x in fruits:
+    print(x)
+for letter in "icta":
+    print(letter)
+mylist = [34,56,78,23]
+for i in mylist:
+    if(i%2==0):
+        print(i)  
