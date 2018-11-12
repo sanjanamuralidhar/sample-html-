c621e6bbac47a64448fb3fbd5da2c8be26220c2a
sanjanamuralidhar <44828329+sanjanamuralidhar@users.noreply.github.com>
2018-11-12 16:14:09 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,12 @@
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = int(input("enter a number"))
+if(a>b):
+    if(a>b):
+        print ("largest is",a)
+    else:
+        print ("largest is",b)
+if(b>c):
+    print ("largest is:",b)
+else:
+    print ("largest is",c)
