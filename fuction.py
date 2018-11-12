598ce5e458163165992726f49c790043732a355e
sanjanamuralidhar <44828329+sanjanamuralidhar@users.noreply.github.com>
2018-11-12 16:13:18 +0530

    Add files via upload

---
+++ fuction.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    z = x+y
+    result=z
+    print("sum is",z)
+def mult(x,y):
+    z=x*y
+    print("product is",z)
+def sub(x,y):
+    z=x-y
+    print("diff is ",z)
+def div(x,y):
+    z=x/y
+    print("quent is",z)
+a=int(input("enter a number "))
+b = int (input("enter num2  "))
+
+result=add(a,b)
+mult(a,b)
+sub(a,b)
+div(a,b)
